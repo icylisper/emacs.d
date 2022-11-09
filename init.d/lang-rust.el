e7b75d94e0442ef4172291a7cd5a8a05850d5dd0
icylisper <icy@lisper.cc>
2022-11-09 07:45:44 -0800

    use rustic-mode with eglot

--- init.d/lang-rust.el
+++ init.d/lang-rust.el
@@ -21,3 +21,33 @@
 	("C-c C-b" . cargo-process-build)
 	("C-c C-k" . cargo-process-run)
 	("C-c C-r" . cargo-process-run)))
+
+
+(defun imenu-rust-fn ()
+    (setq imenu-generic-expression
+          '((nil
+             "\\(^\\s-*(fn +\\)\\(\\_<.+\\_>\\)" 2))))
+
+(add-hook 'rust-mode-hook #'imenu-rust-fn)
+
+
+(el-get-bundle rustic)
+(el-get-bundle eglot)
+
+(use-package rustic
+  :config
+  (setq rustic-lsp-client 'eglot
+	lsp-rust-server 'rust-analyzer
+	rustic-format-on-save nil
+	rustic-lsp-server 'rust-analyzer
+	rustic-flycheck-setup-mode-line-p nil
+	eglot-send-changes-idle-time (* 60 60))
+  (add-hook 'eglot-managed-mode-hook (lambda ()
+				       (eldoc-mode -1)
+				       (flymake-mode -1))))
+
+(defun rustic-mode-auto-save-hook ()
+  "Enable auto-saving in rustic-mode buffers."
+  (when buffer-file-name
+    (setq-local compilation-ask-about-save nil)))
+(add-hook 'rustic-mode-hook 'rustic-mode-auto-save-hook)
